ef526f80640d6cda2f080e223f203d63e4892196
Sarycodes <abdulrahmonshopade@gmail.com>
2022-11-19 16:29:40 -0800

    Added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 success
+ */
+int main(void)
+{
+  int n;
+  char m;
+
+  for (n = '0'; n <= '9'; n++)
+    {
+      putchar(n);
+    }
+  for (m = 'a'; m <= 'f'; m++)
+    {
+      putchar(m);
+    }
+  putchar('\n');
+  return (0);
+
+}
