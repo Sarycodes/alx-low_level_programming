a810f48e73f4c91305ee83f8e0b2d29474e2c706
Sarycodes <abdulrahmonshopade@gmail.com>
2022-11-19 16:26:37 -0800

    Added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 success
+ */
+int main(void)
+{
+  char n;
+
+  for (n = 'a'; n <= 'z'; n++)
+  {
+    if (n != 'q' && n != 'e')
+    {
+      putchar(n);
+    }
+  }
+  putchar('\n');
+  return (0);
+}
