b9d2736a8a42d10c142b1c26f7807e9e34c2f287
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-09 15:23:24 -0800

    fixed 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,29 +7,29 @@
  */
 int main(void)
 {
-  int a, b, c;
+	int a, b, c;
 
-  for (a = '0'; a <= '9'; a++)
-    {
-      for (b = '0'; b <= '9'; b++)
+	for (a = '0'; a <= '9'; a++)
 	{
-	  for (c ='0'; c <= '9'; c++)
-	    {
-	      if (a < b && b < c)
+		for (b = '0'; b <= '9'; b++)
 		{
-		  putchar(a);
-		  putchar(b);
-		  putchar(c);
+			for (c ='0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
 
-		  if (a != '7')
-                  {
-                      putchar(',');
-                      putchar(' ');
-                  }
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+					if (a != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
 }
