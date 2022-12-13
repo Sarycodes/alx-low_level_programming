aadcc7b67e55294e11cf85415d9cf5203a4e9766
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-14 00:20:19 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - prints the alphabet 10 times, in lowercase, followed by a
- *new line
+ *print_alphabet_x10 - prints the alphabet 10 times, in lowercase,
+ *followed by a new line
  *
  *Return: Always 0 success
  */
