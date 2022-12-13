775addb5f163e1511880125cc40e7b68e6fb4f39
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-14 00:50:38 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *print_sign - prints the sign of a number
+ *
+ *@n: integer input
+ *
+ *Return: Always 0 success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return(0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return(-1);
+	}
+}
