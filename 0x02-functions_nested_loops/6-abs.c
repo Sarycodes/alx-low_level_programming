115128a0c3da6d04fce8a0315c219906d54ed3fa
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-14 01:08:34 +0100

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *abs - computes the absolute value of an integer
+ *@n: integer number
+ *Return: Always 0 success
+ */
+
+int _abs(int n)
+{
+	int abs_value;
+
+	if (n < 0)
+	{
+		abs_value = n * -1;
+	}
+}
