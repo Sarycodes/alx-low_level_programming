04e428fa635c4756698c75098fc3c2011449c5ca
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-14 02:10:22 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
   */
 int print_last_digit(int n)
 {
-	int ld = n % 10
+	int ld = n % 10;
 
 	if (ld < 0)
 	{
-		ld = ld * -1
+		ld = ld * -1;
 	}
 
-	_putchar(ld)
+	_putchar(ld);
 
-	return (ld);
+	return ld;
 }
