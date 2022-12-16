99a320badab6e71c9c885f021e67c59cdc10e4d3
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-16 23:14:46 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul -  a function that multiplies two integers
+ *@a: interger input
+ *@b: interger input
+ *Return: sum of a&b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
