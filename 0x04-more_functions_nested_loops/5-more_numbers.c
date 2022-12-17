9f9898c454b5e3ddc60b9f1ceca149c40d478fb7
Sarycodes <abdulrahmonshopade@gmail.com>
2022-12-17 01:08:56 +0100

    Added 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *more_numbers - a function that prints 10 times the 
+ *more_numbers - a function that prints 10 times the
  *numbers,from 0 to 14, followed by a new line
  *
  *Return: number 0 to 14 for 10times
