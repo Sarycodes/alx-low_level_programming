3381a4b8e0c8c5997978761a99c24e02c2b4d711
Sarycodes <abdulrahmonshopade@gmail.com>
2023-01-02 16:06:29 -0800

    Ädded 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes.
+ *
+ * @s: char pointer
+ *
+ * @accept: char pointer
+ *
+ * Return: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		for (; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+	}
+	return (0);
+}
