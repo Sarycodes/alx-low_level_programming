9427c80033a57e6c437436d6a054e68341b42955
Sarycodes <abdulrahmonshopade@gmail.com>
2023-01-04 13:50:42 -0800

    Added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural nums n to 98
+ *
+ * @n: int val
+ *
+ * Return: n to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+
+			if (n = 98)
+			{
+				printf("%d", n);
+			}
+		}
+
+		printf("\n");
+
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+
+			if (n = 98)
+			{
+				printf("%d", n);
+			}
+		}
+
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+		printf("98\n");
+	}
+}
