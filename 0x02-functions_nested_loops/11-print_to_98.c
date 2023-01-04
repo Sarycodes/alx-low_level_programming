a0c5dd2548232c4826bc8eac44452f71a651c76b
Sarycodes <abdulrahmonshopade@gmail.com>
2023-01-04 13:56:55 -0800

    Fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 
-			if (n = 98)
+			if (n == 98)
 			{
 				printf("%d", n);
 			}
@@ -32,7 +32,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 
-			if (n = 98)
+			if (n == 98)
 			{
 				printf("%d", n);
 			}
