bfe8bcd24177f70a608f8b4ba28a2298af392d80
Sarycodes <abdulrahmonshopade@gmail.com>
2023-01-05 14:56:27 -0800

    fixed an error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (s[0] == '\0')
+	{
+		_putchar('\n')';
+	}
+	else if (*s);
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
+	else
 	_putchar('\n');
 }
