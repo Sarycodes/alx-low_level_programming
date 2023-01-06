0a85b7a4211006b1d4dd14263f0b8748708992a9
Sarycodes <abdulrahmonshopade@gmail.com>
2023-01-05 17:22:40 -0800

    Added 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,14 @@
 /**
  * factorial - a function that returns the factorial of a given number
  * @n: input
- * return: fartorial of the number
+ * Return: fartorial of the number
  */
 
-int factorial(int n)
+unsigned int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	if (n == 0)
+		return (1);
 	return (n * factorial(n - 1));
 }
